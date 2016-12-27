742316d6e5c6bb6a56a8f180dc25a6a1363128ab
Simon Pasquier <spasquier@mirantis.com>
2016-12-27 15:06:51 +0100

    Don't return check metric for unknown services
    The check_openstack_api plugin skips unsupported services in the catalog
    instead of returning unknown status.

--- collectd/files/plugin/check_openstack_api.py
+++ collectd/files/plugin/check_openstack_api.py
@@ -68,25 +68,24 @@ class APICheckPlugin(openstack.CollectdPlugin):
             if name not in self.CHECK_MAP:
                 self.logger.notice(
                     "No check found for service '%s', skipping it" % name)
-                status = self.UNKNOWN
-                check = {}
+                continue
+
+            check = self.CHECK_MAP[name]
+            url = self._service_url(service['url'], check['path'])
+            r = self.raw_get(url, token_required=check.get('auth', False))
+
+            if r is None or r.status_code not in check['expect']:
+                def _status(ret):
+                    return 'N/A' if r is None else r.status_code
+
+                self.logger.notice(
+                    "Service %s check failed "
+                    "(returned '%s' but expected '%s')" % (
+                        name, _status(r), check['expect'])
+                )
+                status = self.FAIL
             else:
-                check = self.CHECK_MAP[name]
-                url = self._service_url(service['url'], check['path'])
-                r = self.raw_get(url, token_required=check.get('auth', False))
-
-                if r is None or r.status_code not in check['expect']:
-                    def _status(ret):
-                        return 'N/A' if r is None else r.status_code
-
-                    self.logger.notice(
-                        "Service %s check failed "
-                        "(returned '%s' but expected '%s')" % (
-                            name, _status(r), check['expect'])
-                    )
-                    status = self.FAIL
-                else:
-                    status = self.OK
+                status = self.OK
 
             yield {
                 'service': check.get('name', name),
