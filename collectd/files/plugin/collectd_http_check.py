e30fb971771c88e8ecd9229cbbfc17dad95c3ec1
Swann Croiset <scroiset@mirantis.com>
2017-03-07 10:44:50 +0100

    Introduce an asynchronous http_check plugin
    This plugin replaces the legacy http_check plugin.
    
    Change-Id: If6f6fb6d8a660f172d2947297980fb138ff6b50a

--- collectd/files/plugin/collectd_http_check.py
+++ collectd/files/plugin/collectd_http_check.py
@@ -29,18 +29,25 @@ class HTTPCheckPlugin(base.Base):
     def __init__(self, *args, **kwargs):
         super(HTTPCheckPlugin, self).__init__(*args, **kwargs)
         self.plugin = NAME
-        self.session = requests.Session()
-        self.session.mount(
-            'http://',
-            requests.adapters.HTTPAdapter(max_retries=self.max_retries)
-        )
-        self.session.mount(
-            'https://',
-            requests.adapters.HTTPAdapter(max_retries=self.max_retries)
-        )
         self.urls = {}
         self.expected_codes = {}
         self.expected_contents = {}
+
+        self.timeout = 3
+        self.max_retries = 2
+
+        self.interval = base.INTERVAL
+        self.polling_interval = base.INTERVAL
+
+        self.sessions = {}
+        self._threads = {}
+
+    def shutdown_callback(self):
+        for tid, t in self._threads.items():
+            if t.is_alive():
+                self.logger.info('Waiting for {} thread to finish'.format(tid))
+                t.stop()
+                t.join()
 
     def config_callback(self, config):
         super(HTTPCheckPlugin, self).config_callback(config)
@@ -52,10 +59,24 @@ class HTTPCheckPlugin(base.Base):
             elif node.key == 'ExpectedContent':
                 self.expected_contents[node.values[0]] = node.values[1]
 
-    def itermetrics(self):
         for name, url in self.urls.items():
+            session = requests.Session()
+            session.mount(
+                'http://',
+                requests.adapters.HTTPAdapter(max_retries=self.max_retries)
+            )
+            if url.startswith('https'):
+                session.mount(
+                    'https://',
+                    requests.adapters.HTTPAdapter(max_retries=self.max_retries)
+                )
+            self.sessions[name] = session
+
+    def check_url(self, name, url):
+
+        def get():
             try:
-                r = self.session.get(url, timeout=self.timeout)
+                r = self.sessions[name].get(url, timeout=self.timeout)
             except Exception as e:
                 self.logger.warning("Got exception for '{}': {}".format(
                     url, e)
@@ -83,8 +104,30 @@ class HTTPCheckPlugin(base.Base):
                                 'Content "{}" does not match "{}"'.format(
                                     r.content[0:30], expected_content
                                 ))
+            return [status]
 
-            yield {'type_instance': name, 'values': status }
+        if url not in self._threads:
+            t = base.AsyncPoller(self.collectd,
+                                 get,
+                                 self.polling_interval,
+                                 url)
+            t.start()
+            self._threads[url] = t
+
+        t = self._threads[url]
+        if not t.is_alive():
+            self.logger.warning("Unexpected end of the thread {}".format(
+                t.name))
+            del self._threads[url]
+            return []
+
+        return t.results
+
+    def itermetrics(self):
+        for name, url in self.urls.items():
+            r = self.check_url(name, url)
+            if r:
+                yield {'type_instance': name, 'values': r}
 
 
 plugin = HTTPCheckPlugin(collectd, disable_check_metric=True)
@@ -105,9 +148,15 @@ def read_callback():
 if __name__ == '__main__':
     plugin.urls['google_ok'] = 'https://www.google.com'
     plugin.urls['google_fail'] = 'https://www.google.com/not_found'
+    plugin.urls['no_network'] = 'https://127.0.0.2:999'
     plugin.expected_codes['google_ok'] = 200
-    plugin.expected_codes['github_fail'] = 200
+    plugin.expected_codes['google_fail'] = 200
+    collectd.load_configuration(plugin)
     plugin.read_callback()
+    import time
+    time.sleep(base.INTERVAL)
+    plugin.read_callback()
+    plugin.shutdown_callback()
 else:
     collectd.register_config(config_callback)
     collectd.register_notification(notification_callback)
